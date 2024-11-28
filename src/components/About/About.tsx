347a39edf8d1c8951b50545b1c01717fad25b9f5
Ekansh Singh <110751935+singh-ekansh@users.noreply.github.com>
2024-11-29 02:07:24 +0530

    simple changes

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -4,133 +4,85 @@ import htmlIcon from "../../assets/html-icon.svg";
 import cssIcon from "../../assets/css-icon.svg";
 import jsIcon from "../../assets/js-icon.svg";
 import reactIcon from "../../assets/react-icon.svg";
-import mongoIcon from "../../assets/mongo-icon.svg"; // Add your MongoDB logo file here
-import wixIcon from "../../assets/wix-icon.svg"; // Add your Wix logo file here
+import mongoIcon from "../../assets/mongo-icon.svg";
+import wixIcon from "../../assets/wix-icon.svg";
 import wordpressIcon from "../../assets/wordpress.svg";
-import pythonIcon from "../../assets/python-icon.svg"; // Add your Python logo file here
-import gitIcon from "../../assets/git-icon.svg"; // Add your Git logo file here
-import jetpackIcon from "../../assets/jetpack-icon.svg"; // Add your Jetpack logo file here
-import cppIcon from "../../assets/cpp-icon.svg"; // Add your C++ logo file here
-import figmaIcon from "../../assets/figma-icon.svg"; // Add your Figma logo file here
-import vscodeIcon from "../../assets/vscode-icon.svg"; // Add your Visual Studio Code logo file here
-import intellijIcon from "../../assets/intellij-icon.svg"; // Add your IntelliJ logo file here
-import ScrollAnimation from "react-animate-on-scroll";
+import pythonIcon from "../../assets/python-icon.svg";
+import gitIcon from "../../assets/git-icon.svg";
+import jetpackIcon from "../../assets/jetpack-icon.svg";
+import cppIcon from "../../assets/cpp-icon.svg";
+import figmaIcon from "../../assets/figma-icon.svg";
+import vscodeIcon from "../../assets/vscode-icon.svg";
+import intellijIcon from "../../assets/intellij-icon.svg";
 
 export function About() {
   return (
     <Container id="about">
       <div className="about-text">
-        <ScrollAnimation animateIn="fadeInLeft">
-          <h2>About me</h2>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInLeft" delay={0.1 * 1000}>
-          <p>
-            Hi there! I'm Ekansh Singh, an aspiring full-stack developer with a
-            passion for crafting innovative and efficient digital solutions.
-            With expertise in HTML, CSS, JavaScript, React.js, and MongoDB, I
-            specialize in building seamless, user-friendly web applications that
-            cater to modern needs.
-          </p>
-        </ScrollAnimation>
-        <ScrollAnimation
-          animateIn="fadeInLeft"
-          delay={0.2 * 1000}
-          style={{ marginTop: "2rem", marginBottom: "2rem" }}
-        >
-          <p>
-            Beyond custom development, I bring experience in leveraging CMS
-            platforms like WordPress, ensuring my clients have the flexibility
-            to manage their websites effortlessly. Whether it’s designing
-            intuitive user interfaces or implementing robust back-end systems,
-            I’m committed to delivering solutions that exceed expectations.
-          </p>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInLeft" delay={0.3 * 1000}>
-          <p>
-            Let’s connect to bring your ideas to life with creativity and
-            precision!
-          </p>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInLeft" delay={0.4 * 1000}>
-          <h3>Here are my main skills:</h3>
-        </ScrollAnimation>
+        <h2>About me</h2>
+        <p>
+          Hi there! I'm Ekansh Singh, an aspiring full-stack developer with a
+          passion for crafting innovative and efficient digital solutions. With
+          expertise in HTML, CSS, JavaScript, React.js, and MongoDB, I
+          specialize in building seamless, user-friendly web applications that
+          cater to modern needs.
+        </p>
+        <p style={{ marginTop: "2rem", marginBottom: "2rem" }}>
+          Beyond custom development, I bring experience in leveraging CMS
+          platforms like WordPress, ensuring my clients have the flexibility to
+          manage their websites effortlessly. Whether it’s designing intuitive
+          user interfaces or implementing robust back-end systems, I’m committed
+          to delivering solutions that exceed expectations.
+        </p>
+        <p>Let’s connect to bring your ideas to life with creativity and precision!</p>
+        <h3>Here are my main skills:</h3>
         <div className="hard-skills">
           <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.1 * 1000}>
-              <img src={htmlIcon} alt="HTML" />
-            </ScrollAnimation>
+            <img src={htmlIcon} alt="HTML" />
           </div>
           <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.12 * 1000}>
-              <img src={cssIcon} alt="CSS" />
-            </ScrollAnimation>
+            <img src={cssIcon} alt="CSS" />
           </div>
           <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.13 * 1000}>
-              <img src={jsIcon} alt="JavaScript" />
-            </ScrollAnimation>
+            <img src={jsIcon} alt="JavaScript" />
           </div>
           <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.14 * 1000}>
-              <img src={reactIcon} alt="React" />
-            </ScrollAnimation>
+            <img src={reactIcon} alt="React" />
           </div>
           <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.15 * 1000}>
-              <img src={mongoIcon} alt="MongoDB" />
-            </ScrollAnimation>
+            <img src={mongoIcon} alt="MongoDB" />
           </div>
           <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.16 * 1000}>
-              <img src={wixIcon} alt="Wix" />
-            </ScrollAnimation>
+            <img src={wixIcon} alt="Wix" />
           </div>
           <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.17 * 1000}>
-              <img src={wordpressIcon} alt="WordPress" />
-            </ScrollAnimation>
+            <img src={wordpressIcon} alt="WordPress" />
           </div>
           <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.18 * 1000}>
-              <img src={pythonIcon} alt="Python" />
-            </ScrollAnimation>
+            <img src={pythonIcon} alt="Python" />
           </div>
           <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.19 * 1000}>
-              <img src={gitIcon} alt="Git" />
-            </ScrollAnimation>
+            <img src={gitIcon} alt="Git" />
           </div>
           <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.2 * 1000}>
-              <img src={jetpackIcon} alt="Jetpack" />
-            </ScrollAnimation>
+            <img src={jetpackIcon} alt="Jetpack" />
           </div>
           <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.21 * 1000}>
-              <img src={cppIcon} alt="C++" />
-            </ScrollAnimation>
+            <img src={cppIcon} alt="C++" />
           </div>
           <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.22 * 1000}>
-              <img src={figmaIcon} alt="Figma" />
-            </ScrollAnimation>
+            <img src={figmaIcon} alt="Figma" />
           </div>
           <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.23 * 1000}>
-              <img src={vscodeIcon} alt="Visual Studio Code" />
-            </ScrollAnimation>
+            <img src={vscodeIcon} alt="Visual Studio Code" />
           </div>
           <div className="hability">
-            <ScrollAnimation animateIn="fadeInUp" delay={0.24 * 1000}>
-              <img src={intellijIcon} alt="IntelliJ" />
-            </ScrollAnimation>
+            <img src={intellijIcon} alt="IntelliJ" />
           </div>
         </div>
       </div>
       <div className="about-image">
-        <ScrollAnimation animateIn="fadeInRight" delay={0.2 * 1000}>
-          <img src={EkanshSingh} alt="Ekansh Singh" />
-        </ScrollAnimation>
+        <img src={EkanshSingh} alt="Ekansh Singh" />
       </div>
     </Container>
   );
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,65 +1,61 @@
-import { BrowserRouter } from "react-router-dom"
-import { Container } from "./styles"
-import ScrollAnimation from "react-animate-on-scroll"
-import Illustration from "../../assets/illustration.svg"
-import { NavHashLink } from "react-router-hash-link"
-import linkedin from '../../assets/linkedin.svg'
-import githubIcon from '../../assets/github.svg'
-import whatsapp from '../../assets/whatsapp.svg'
-import Hello from '../../assets/Hello.gif'
-import telegram from '../../assets/telegram.svg'
+import { BrowserRouter } from "react-router-dom";
+import { Container } from "./styles";
+import Illustration from "../../assets/illustration.svg";
+import { NavHashLink } from "react-router-hash-link";
+import linkedin from "../../assets/linkedin.svg";
+import githubIcon from "../../assets/github.svg";
+import whatsapp from "../../assets/whatsapp.svg";
+import Hello from "../../assets/Hello.gif";
+import ScrollAnimation from "react-animate-on-scroll";
+
 export function Hero() {
   return (
     <Container id="home">
       <div className="hero-text">
-        <ScrollAnimation animateIn="fadeInUp">
-          <p>Hello <img src={Hello} alt="Hello" width="20px"/>, I'm</p>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInUp" delay={0.2 * 1000}>
-          <h1>Ekansh Singh</h1>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInUp" delay={0.4 * 1000}>
-          <h3>Front-End Development | SEO | Management</h3>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInUp" delay={0.6 * 1000}>
-          <p className="small-resume">Bachelor of Engineering | Electronics and Computer Engineering 
-              <br/>Thapar Institute of Engineering and Technology Patiala</p>
-        </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInUp" delay={0.8 * 1000}>
-          <BrowserRouter>
-            <NavHashLink smooth to="#contact" className="button">Contact</NavHashLink>
-          </BrowserRouter>
-        </ScrollAnimation>
+        <p>
+          Hello <img src={Hello} alt="Hello" width="20px" />, I'm
+        </p>
+        <h1>Ekansh Singh</h1>
+        <h3>Front-End Development | SEO | Management</h3>
+        <p className="small-resume">
+          Bachelor of Engineering | Electronics and Computer Engineering
+          <br />
+          Thapar Institute of Engineering and Technology Patiala
+        </p>
+        <BrowserRouter>
+          <NavHashLink smooth to="#contact" className="button">
+            Contact
+          </NavHashLink>
+        </BrowserRouter>
         <ScrollAnimation animateIn="fadeInUp" delay={1 * 1000}>
-      <div className="social-media"><a
-        href="https://www.linkedin.com/in/ekanshsinghin/"
-        target="_blank"
-        rel="noreferrer"
-      >
-        <img src={linkedin} alt="Linkedin" />
-      </a>
-        <a
-          href="https://github.com/singh-ekansh"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={githubIcon} alt="GitHub" />
-        </a>
-        <a
-          href="https://api.whatsapp.com/send/?phone=%2B917380587032&text=Hello+Ekansh%2C+I+am+interested+in+your+services&app_absent=0"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={whatsapp} alt="Whatsapp" />
-        </a>
-       </div>
+          <div className="social-media">
+            <a
+              href="https://www.linkedin.com/in/ekanshsinghin/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={linkedin} alt="Linkedin" />
+            </a>
+            <a
+              href="https://github.com/singh-ekansh"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={githubIcon} alt="GitHub" />
+            </a>
+            <a
+              href="https://api.whatsapp.com/send/?phone=%2B917380587032&text=Hello+Ekansh%2C+I+am+interested+in+your+services&app_absent=0"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={whatsapp} alt="Whatsapp" />
+            </a>
+          </div>
         </ScrollAnimation>
       </div>
       <div className="hero-image">
-        <ScrollAnimation animateIn="fadeInRight" delay={1 * 1000}>
-          <img src={Illustration} alt="Ilustração" />
-        </ScrollAnimation>
+        <img src={Illustration} alt="Illustration" />
       </div>
     </Container>
-  )
+  );
 }
