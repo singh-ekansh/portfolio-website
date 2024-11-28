a13b5899ea824516b386ae30f14530177969e431
Ekansh Singh <110751935+singh-ekansh@users.noreply.github.com>
2024-11-29 01:39:16 +0530

    simple changes

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,5 +1,5 @@
 import { Container } from "./styles";
-import VinayakSingh from "../../assets/VinayakSingh.png";
+import EkanshSingh from "../../assets/EkanshSingh.png";
 import wordpress from "../../assets/wordpress.svg";
 import shopify from "../../assets/shopify.svg";
 import htmlIcon from "../../assets/html-icon.svg";
@@ -21,16 +21,16 @@ export function About() {
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInLeft" delay={0.1 * 1000}>
           <p>
-            Hi there! I'm Vinayak, a website developer with a passion for creating custom online experiences for my clients. With a skill set including HTML, CSS, JavaScript, and React, I have the tools to bring any website vision to life.
+          Hi there! I'm Ekansh Singh, an aspiring full-stack developer with a passion for crafting innovative and efficient digital solutions. With expertise in HTML, CSS, JavaScript, React.js, and MongoDB, I specialize in building seamless, user-friendly web applications that cater to modern needs.
           </p>
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInLeft" delay={0.2 * 1000} style={{ marginTop: "2rem", marginBottom: "2rem" }}>
           <p>
-            But my services go beyond just custom development - I'm also proficient in using CMS systems like WordPress and Shopify, making it easy for my clients to take control of their own websites and keep them up to date.</p>
+          Beyond custom development, I bring experience in leveraging CMS platforms like WordPress, ensuring my clients have the flexibility to manage their websites effortlessly. Whether it’s designing intuitive user interfaces or implementing robust back-end systems, I’m committed to delivering solutions that exceed expectations.</p>
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInLeft" delay={0.3 * 1000}>
           <p>
-            So if you're in need of a new website or just looking to revamp your current online presence, I'd love to chat and see how I can help. Let's bring your website dreams to reality together!</p>
+          Let’s connect to bring your ideas to life with creativity and precision!</p>
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInLeft" delay={0.4 * 1000}>
           <h3>Here are my main skills:</h3>
@@ -90,7 +90,7 @@ export function About() {
       </div>
       <div className="about-image">
         <ScrollAnimation animateIn="fadeInRight" delay={0.20 * 1000}>
-          <img src={VinayakSingh} alt="Vinayak Singh" />
+          <img src={EkanshSingh} alt="Ekansh Singh" />
         </ScrollAnimation>
       </div>
     </Container>
