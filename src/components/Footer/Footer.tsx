bc4ab08c32c09e50e7f84bc23655d34a7c0fb67e
Ekansh Singh <110751935+singh-ekansh@users.noreply.github.com>
2024-12-22 15:57:36 +0530

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,8 +3,8 @@ import reactIcon from '../../assets/react-icon.svg'
 import linkedin from '../../assets/linkedin.svg'
 import githubIcon from '../../assets/github.svg'
 import whatsapp from '../../assets/whatsapp.svg'
-import telegram from '../../assets/telegram.svg'
-import instagramIcon from '../../assets/instagram.svg'
+// import telegram from '../../assets/telegram.svg'
+// import instagramIcon from '../../assets/instagram.svg'
 // import discordIcon from '../../assets/discord.png'
 
 export function Footer() {
@@ -17,7 +17,7 @@ export function Footer() {
       <div>
         <p>
           This Website was made with <img src={reactIcon} alt="React" />
-          {/* <span>❤️</span> */}
+          { <span>❤️</span> }
         </p>
       </div>
       <div className="social-media">
