35df2b12b3afc4438ad6fc8146195482ae6c4b17
Ekansh Singh <110751935+singh-ekansh@users.noreply.github.com>
2024-12-22 16:05:48 +0530

    Update Project.tsx

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -10,10 +10,6 @@ export function Project() {
     <Container id="project">
       <h2>My Projects</h2>
       <div className="projects">
-
-        
-
-        
         <ScrollAnimation animateIn="flipInX">
           <div className="project">
             <header>
@@ -100,33 +96,6 @@ export function Project() {
                 <li>Streamlit</li>
                 <li>Pandas</li>
                
-              </ul>
-            </footer>
-          </div>
-        </ScrollAnimation>
-
-        <ScrollAnimation animateIn="flipInX">
-          <div className="project">
-            <header>
-              <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
-              <div className="project-links">
-                <a href="https://velocityai.vinayaksingh.com/" target="_blank" rel="noreferrer">
-                  <img src={externalLink} alt="Visit site" />
-                </a>
-              </div>
-            </header>
-            <div className="body">
-              <h3>VeloCityAI</h3>
-              <p>
-                VelocityAI is a website that allows users to get instant answers to any question and generate stunning AI pictures using OpenAI's GPT-3 and DALL-E model.With VelocityAI, users can type in text-based prompts and receive a corresponding response.
-              </p>
-            </div>
-            <footer>
-              <ul className="tech-list">
-                <li>React</li>
-                <li>Typescript</li>
-                <li>Html</li>
-                <li>css</li>
               </ul>
             </footer>
           </div>
